5e45368bdf6dc9c0ac6cbbffeca2cd24941a381b
utdmn2 <utdmn2@gmail.com>
2021-11-06 09:39:37 +0000

    [Add]管理者側会員側devise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  devise_for :users
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords,], controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
