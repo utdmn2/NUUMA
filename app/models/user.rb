868f25721c07f2190de25544065097c54c7f7b15
utdmn2 <utdmn2@gmail.com>
2021-11-14 07:58:19 +0000

    [Add]いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   #新規登録チェックボックスvalidation
   validates :agreement_terms, allow_nil: false, acceptance: true, on: :create
   validates :email, presence: true, uniqueness: true
 
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   attachment :profile_image
 
