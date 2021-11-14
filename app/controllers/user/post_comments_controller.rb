6a11a7a36b75901ad51afa9f791deaccb3582ef2
utdmn2 <utdmn2@gmail.com>
2021-11-14 07:07:14 +0000

    Add

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -1,14 +1,15 @@
 class User::PostCommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:post_id])
+    post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = @post.id
+    comment.post_id = post.id
     comment.save
-    redirect_to post_path(@post)
+    redirect_to post_path(post)
   end
 
   def destroy
+    @post = Post.find(params[:id])
     PostComment.find_by(id: params[:id]).destroy
     redirect_to post_path(params[:post_id])
   end
