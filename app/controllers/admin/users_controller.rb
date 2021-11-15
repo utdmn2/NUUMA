949f1d7ad0b90a0db5e9daeb0e183117585754f7
utdmn2 <utdmn2@gmail.com>
2021-11-15 09:29:05 +0000

    [Add]管理者側会員ステータス

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,9 +3,21 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "ステータス変更ずみ"
+    redirect_to "/admin/users"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:is_deleted)
+  end
 end
