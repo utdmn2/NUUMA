27748e3621fee41084fecec1a9246c96b06fb0af
utdmn2 <utdmn2@gmail.com>
2021-11-16 23:39:56 +0000

    [Finish]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,16 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
   attachment :profile_image
 
 end
