58197525267b989558c88d85b35bfea0d06d5dd7
utdmn2 <utdmn2@gmail.com>
2021-11-18 12:02:40 +0000

    エラーメッセージ日本語化

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,7 +26,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { maximum: 1000 }
-  # validates :category_ids, presence: true
+  validates :category_ids, presence: true
 
 
   attachment :image
